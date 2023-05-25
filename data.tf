d0ee9faf10b42d633d9f3a72dc7ef989a7a9a4c4
tynchtyk642 <tynchtyk642@gmail.com>
2023-05-25 20:33:15 +0600

    first commit

---
+++ data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.0.20230503.0-*"]
+  }
+}
---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.env}-vpc-${var.name}"
+  }
+}
+
+
+resource "aws_instance" "condition" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "${var.env}-ec2-${var.name}"
+  }
+}
